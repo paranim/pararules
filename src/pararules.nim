03e5101e671c1f0e3876b68ee6d7fd37e20cc632
oakes <zach@sekao.net>
2020-01-10 18:48:12 -0500

    Experiment with variants and whatnot

--- src/pararules.nim
+++ src/pararules.nim
@@ -1,9 +1,11 @@
 type
   Field* {.pure.} = enum
     None, Identifier, Attribute, Value
+  WME*[T] = tuple[id: int, attr: T, value: T]
   AlphaNode*[T] = object
     testField*: Field
     testValue*: T
+    outputMemory*: seq[WME[T]]
     children*: seq[AlphaNode[T]]
   Session*[T] = object
     rootNode*: AlphaNode[T]
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,10 +1,24 @@
 import unittest
 import pararules
 
+type
+  DataType = enum
+    String, Int
+  Data = object
+    case kind: DataType
+    of String: strVal: string
+    of Int: intVal: int
+
+proc newStr(val: string): Data =
+  Data(kind: String, strVal: val)
+
+proc newInt(val: int): Data =
+  Data(kind: Int, intVal: val)
+
 test "can create session":
-  var session = Session[string]()
-  var node = AlphaNode[string]()
-  node.testField = Field.Attribute
-  node.testValue = "color"
+  var session = Session[Data]()
+  var node = AlphaNode[Data](testField: Field.Attribute, testValue: newStr("color"))
   session.rootNode.children.add(node)
+  var wme = (id: 1, attr: newStr("size"), value: newInt(100))
+  session.rootNode.outputMemory.add(wme)
   echo session
