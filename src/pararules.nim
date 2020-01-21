61325c304a20d107bdfe3dbb9d4c8ef7f611bf8d
oakes <zach@sekao.net>
2020-01-20 19:05:36 -0500

    Allow id, attr, and value to be different types

--- src/pararules.nim
+++ src/pararules.nim
@@ -4,14 +4,19 @@ type
   # facts
   Field = enum
     Identifier, Attribute, Value
-  Fact[T] = tuple[id: T, attr: T, value: T]
+  Fact[I, A, V] = tuple[id: I, attr: A, value: V]
   # alpha network
-  AlphaNode[T] = ref object
-    testField: Field
-    testValue: T
-    facts: seq[Fact[T]]
-    successors: seq[JoinNode[T]]
-    children: seq[AlphaNode[T]]
+  AlphaNode[I, A, V] = ref object
+    case field: Field
+    of Identifier:
+      id: I
+    of Attribute:
+      attr: A
+    of Value:
+      value: V
+    facts: seq[Fact[I, A, V]]
+    successors: seq[JoinNode[I, A, V]]
+    children: seq[AlphaNode[I, A, V]]
   # beta network
   TestAtJoinNode = object
     alphaField: Field
@@ -19,38 +24,46 @@ type
     condition: int
   NodeType = enum
     Root, Partial, Full
-  MemoryNode[T] = ref object
-    parent: JoinNode[T]
-    children: seq[JoinNode[T]]
-    facts*: seq[seq[Fact[T]]]
+  MemoryNode[I, A, V] = ref object
+    parent: JoinNode[I, A, V]
+    children: seq[JoinNode[I, A, V]]
+    facts*: seq[seq[Fact[I, A, V]]]
     case nodeType: NodeType
     of Full:
-      production: Production[T]
+      production: Production[I, A, V]
     else:
       nil
-  JoinNode[T] = ref object
-    parent: MemoryNode[T]
-    children: seq[MemoryNode[T]]
-    alphaNode: AlphaNode[T]
+  JoinNode[I, A, V] = ref object
+    parent: MemoryNode[I, A, V]
+    children: seq[MemoryNode[I, A, V]]
+    alphaNode: AlphaNode[I, A, V]
     tests: seq[TestAtJoinNode]
   # session
   Vars[T] = Table[string, T]
   Var* = object
     name*: string
     field: Field
-  Condition[T] = object
-    nodes: seq[AlphaNode[T]]
+  Condition[I, A, V] = object
+    nodes: seq[AlphaNode[I, A, V]]
     vars: seq[Var]
-  Production[T] = object
-    conditions: seq[Condition[T]]
-    callback: proc (vars: Vars[T])
-  Session[T] = object
-    alphaNode: AlphaNode[T]
-    betaNode: MemoryNode[T]
+  Callback[I, A, V] = proc (ids: Vars[I], attrs: Vars[A], values: Vars[V])
+  Production[I, A, V] = object
+    conditions: seq[Condition[I, A, V]]
+    callback: Callback[I, A, V]
+  Session[I, A, V] = object
+    alphaNode: AlphaNode[I, A, V]
+    betaNode: MemoryNode[I, A, V]
+
+proc `==`(x, y: AlphaNode): bool =
+  x.field == y.field and
+    (case x.field:
+     of Identifier: x.id == y.id
+     of Attribute: x.attr == y.attr
+     of Value: x.value == y.value)
 
 proc addNode(node: AlphaNode, newNode: AlphaNode): AlphaNode =
   for child in node.children:
-    if child.testField == newNode.testField and child.testValue == newNode.testValue:
+    if child == newNode:
       return child
   node.children.add(newNode)
   return newNode
@@ -60,27 +73,36 @@ proc addNodes(session: Session, nodes: seq[AlphaNode]): AlphaNode =
   for node in nodes:
     result = result.addNode(node)
 
-proc addCondition*[T](production: var Production[T], id: Var or T, attr: Var or T, value: Var or T) =
-  var condition = Condition[T]()
+proc newIdAlphaNode[I, A, V](id: I): AlphaNode[I, A, V] =
+  AlphaNode[I, A, V](field: Identifier, id: id)
+
+proc newAttrAlphaNode[I, A, V](attr: A): AlphaNode[I, A, V] =
+  AlphaNode[I, A, V](field: Attribute, attr: attr)
+
+proc newValueAlphaNode[I, A, V](value: V): AlphaNode[I, A, V] =
+  AlphaNode[I, A, V](field: Value, value: value)
+
+proc addCondition*[I, A, V](production: var Production[I, A, V], id: Var or I, attr: Var or A, value: Var or V) =
+  var condition = Condition[I, A, V]()
   for fieldType in [Field.Identifier, Field.Attribute, Field.Value]:
     case fieldType:
       of Field.Identifier:
-        when id is T:
-          condition.nodes.add AlphaNode[T](testField: fieldType, testValue: id)
+        when id is I:
+          condition.nodes.add(newIdAlphaNode[I, A, V](id))
         else:
           var temp = id
           temp.field = fieldType
           condition.vars.add(temp)
       of Field.Attribute:
-        when attr is T:
-          condition.nodes.add AlphaNode[T](testField: fieldType, testValue: attr)
+        when attr is A:
+          condition.nodes.add(newAttrAlphaNode[I, A, V](attr))
         else:
           var temp = attr
           temp.field = fieldType
           condition.vars.add(temp)
       of Field.Value:
-        when value is T:
-          condition.nodes.add AlphaNode[T](testField: fieldType, testValue: value)
+        when value is V:
+          condition.nodes.add(newValueAlphaNode[I, A, V](value))
         else:
           var temp = value
           temp.field = fieldType
@@ -96,14 +118,14 @@ proc isAncestor(x, y: JoinNode): bool =
       node = node.parent.parent
   false
 
-proc addProduction*[T](session: Session[T], production: Production[T]): MemoryNode[T] =
+proc addProduction*[I, A, V](session: Session[I, A, V], production: Production[I, A, V]): MemoryNode[I, A, V] =
   var joins: Table[string, (Var, int)]
   result = session.betaNode
   let last = production.conditions.len - 1
   for i in 0 .. last:
     var condition = production.conditions[i]
     var leafNode = session.addNodes(condition.nodes)
-    var joinNode = JoinNode[T](parent: result, alphaNode: leafNode)
+    var joinNode = JoinNode[I, A, V](parent: result, alphaNode: leafNode)
     for v in condition.vars:
       if joins.hasKey(v.name):
         let (joinVar, condNum) = joins[v.name]
@@ -112,24 +134,57 @@ proc addProduction*[T](session: Session[T], production: Production[T]): MemoryNo
     result.children.add(joinNode)
     leafNode.successors.add(joinNode)
     # successors must be sorted by ancestry (descendents first) to avoid duplicate rule firings
-    leafNode.successors.sort(proc (x, y: JoinNode[T]): int =
+    leafNode.successors.sort(proc (x, y: JoinNode[I, A, V]): int =
       if isAncestor(x, y): 1 else: -1)
-    var newMemNode = MemoryNode[T](parent: joinNode, nodeType: if i == last: Full else: Partial)
+    var newMemNode = MemoryNode[I, A, V](parent: joinNode, nodeType: if i == last: Full else: Partial)
     if newMemNode.nodeType == Full:
       newMemNode.production = production
     joinNode.children.add(newMemNode)
     result = newMemNode
 
 proc performJoinTest(test: TestAtJoinNode, alphaFact: Fact, betaFact: Fact): bool =
-  let arg1 = case test.alphaField:
-    of Field.Identifier: alphaFact[0]
-    of Field.Attribute: alphaFact[1]
-    of Field.Value: alphaFact[2]
-  let arg2 = case test.betaField:
-    of Field.Identifier: betaFact[0]
-    of Field.Attribute: betaFact[1]
-    of Field.Value: betaFact[2]
-  arg1 == arg2
+  let (alphaId, alphaAttr, alphaValue) = alphaFact
+  let (betaId, betaAttr, betaValue) = betaFact
+  case test.alphaField:
+    of Identifier:
+      case test.betaField:
+        of Identifier:
+          alphaId == betaId
+        of Attribute:
+          when alphaId is betaAttr.type:
+            alphaId == betaAttr
+          else:
+            false
+        of Value:
+          alphaId == betaValue
+    of Attribute:
+      case test.betaField:
+        of Identifier:
+          when alphaAttr is betaId.type:
+            alphaAttr == betaId
+          else:
+            false
+        of Attribute:
+          alphaAttr == betaAttr
+        of Value:
+          when alphaAttr is betaValue.type:
+            alphaAttr == betaValue
+          else:
+            false
+    of Value:
+      case test.betaField:
+        of Identifier:
+          when alphaValue is betaId.type:
+            alphaValue == betaId
+          else:
+            false
+        of Attribute:
+          when alphaValue is betaAttr.type:
+            alphaValue == betaAttr
+          else:
+            false
+        of Value:
+          alphaValue == betaValue
 
 proc performJoinTests(tests: seq[TestAtJoinNode], facts: seq[Fact], alphaFact: Fact): bool =
   for test in tests:
@@ -138,45 +193,47 @@ proc performJoinTests(tests: seq[TestAtJoinNode], facts: seq[Fact], alphaFact: F
       return false
   true
 
-proc leftActivation[T](node: MemoryNode[T], facts: seq[Fact[T]], fact: Fact[T])
+proc leftActivation[I, A, V](node: MemoryNode[I, A, V], facts: seq[Fact[I, A, V]], fact: Fact[I, A, V])
 
-proc leftActivation[T](node: JoinNode[T], facts: seq[Fact[T]]) =
+proc leftActivation[I, A, V](node: JoinNode[I, A, V], facts: seq[Fact[I, A, V]]) =
   for alphaFact in node.alphaNode.facts:
     if performJoinTests(node.tests, facts, alphaFact):
       for child in node.children:
         child.leftActivation(facts, alphaFact)
 
-proc leftActivation[T](node: MemoryNode[T], facts: seq[Fact[T]], fact: Fact[T]) =
+proc leftActivation[I, A, V](node: MemoryNode[I, A, V], facts: seq[Fact[I, A, V]], fact: Fact[I, A, V]) =
   var newFacts = facts
   newFacts.add(fact)
   node.facts.add(newFacts)
   if node.nodeType == Full:
     assert node.production.conditions.len == newFacts.len
-    var vars: Vars[T]
+    var ids: Vars[I]
+    var attrs: Vars[A]
+    var values: Vars[V]
     for i in 0 ..< node.production.conditions.len:
       for v in node.production.conditions[i].vars:
         case v.field:
           of Identifier:
-            if vars.hasKey(v.name):
-              assert vars[v.name] == newFacts[i][0]
+            if ids.hasKey(v.name):
+              assert ids[v.name] == newFacts[i][0]
             else:
-              vars[v.name] = newFacts[i][0]
+              ids[v.name] = newFacts[i][0]
           of Attribute:
-            if vars.hasKey(v.name):
-              assert vars[v.name] == newFacts[i][1]
+            if attrs.hasKey(v.name):
+              assert attrs[v.name] == newFacts[i][1]
             else:
-              vars[v.name] = newFacts[i][1]
+              attrs[v.name] = newFacts[i][1]
           of Value:
-            if vars.hasKey(v.name):
-              assert vars[v.name] == newFacts[i][2]
+            if values.hasKey(v.name):
+              assert values[v.name] == newFacts[i][2]
             else:
-              vars[v.name] = newFacts[i][2]
-    node.production.callback(vars)
+              values[v.name] = newFacts[i][2]
+    node.production.callback(ids, attrs, values)
   else:
     for child in node.children:
       child.leftActivation(newFacts)
 
-proc rightActivation[T](node: JoinNode[T], fact: Fact[T]) =
+proc rightActivation[I, A, V](node: JoinNode[I, A, V], fact: Fact[I, A, V]) =
   if node.parent.nodeType == Root:
     for child in node.children:
       child.leftActivation(@[], fact)
@@ -193,11 +250,11 @@ proc rightActivation(node: AlphaNode, fact: Fact) =
 
 proc addFact(node: AlphaNode, fact: Fact, root: bool): bool =
   if not root:
-    let val = case node.testField:
-      of Field.Identifier: fact[0]
-      of Field.Attribute: fact[1]
-      of Field.Value: fact[2]
-    if val != node.testValue:
+    let match = case node.field:
+      of Field.Identifier: fact[0] == node.id
+      of Field.Attribute: fact[1] == node.attr
+      of Field.Value: fact[2] == node.value
+    if not match:
       return false
   for child in node.children:
     if child.addFact(fact, false):
@@ -208,16 +265,16 @@ proc addFact(node: AlphaNode, fact: Fact, root: bool): bool =
 proc addFact*(session: Session, fact: Fact) =
   discard session.alphaNode.addFact(fact, true)
 
-proc newSession*[T](): Session[T] =
-  result.alphaNode = new(AlphaNode[T])
-  result.betaNode = new(MemoryNode[T])
+proc newSession*[I, A, V](): Session[I, A, V] =
+  result.alphaNode = new(AlphaNode[I, A, V])
+  result.betaNode = new(MemoryNode[I, A, V])
 
-proc newProduction*[T](cb: proc (vars: Vars[T])): Production[T] =
+proc newProduction*[I, A, V](cb: Callback[I, A, V]): Production[I, A, V] =
   result.callback = cb
 
 proc print(fact: Fact, indent: int): string
-proc print[T](node: JoinNode[T], indent: int): string
-proc print[T](node: MemoryNode[T], indent: int): string
+proc print[I, A, V](node: JoinNode[I, A, V], indent: int): string
+proc print[I, A, V](node: MemoryNode[I, A, V], indent: int): string
 proc print(node: AlphaNode, indent: int): string
 
 proc print(fact: Fact, indent: int): string =
@@ -226,14 +283,14 @@ proc print(fact: Fact, indent: int): string =
       result &= "  "
   result &= "Fact = {fact} \n".fmt
 
-proc print[T](node: JoinNode[T], indent: int): string =
+proc print[I, A, V](node: JoinNode[I, A, V], indent: int): string =
   for i in 0 ..< indent:
     result &= "  "
   result &= "JoinNode\n"
   for child in node.children:
     result &= print(child, indent+1)
 
-proc print[T](node: MemoryNode[T], indent: int): string =
+proc print[I, A, V](node: MemoryNode[I, A, V], indent: int): string =
   for i in 0 ..< indent:
     result &= "  "
   let cnt = node.facts.len
@@ -251,7 +308,7 @@ proc print(node: AlphaNode, indent: int): string =
   else:
     for i in 0 ..< indent:
       result &= "  "
-    result &= "{node.testField} = {node.testValue} ({cnt})\n".fmt
+    result &= "{node.field} ({cnt})\n".fmt
   for fact in node.facts:
     result &= print(fact, indent+1)
   for child in node.children:
--- tests/test1.nim
+++ tests/test1.nim
@@ -3,6 +3,8 @@ import pararules
 import tables
 
 type
+  Attr = enum
+    Color, LeftOf, RightOf, Height, On, Self
   DataType = enum
     Str, Int
   Data = object
@@ -23,79 +25,100 @@ proc newInt(val: int): Data =
   Data(kind: Int, intVal: val)
 
 test "number of conditions != number of facts":
-  var vars: Table[string, Data]
-  var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "b"), newStr("color"), newStr("blue"))
-  prod.addCondition(Var(name: "y"), newStr("left-of"), Var(name: "z"))
-  prod.addCondition(Var(name: "a"), newStr("color"), newStr("maize"))
-  prod.addCondition(Var(name: "y"), Var(name: "a"), Var(name: "b"))
-  prod.addCondition(Var(name: "x"), newStr("height"), Var(name: "h"))
+  var ids: Table[string, Data]
+  var attrs: Table[string, Attr]
+  var values: Table[string, Data]
+  var prod = newProduction[Data, Attr, Data](
+    proc (i: Table[string, Data], a: Table[string, Attr], v: Table[string, Data]) =
+      ids = i
+      attrs = a
+      values = v
+  )
+  prod.addCondition(Var(name: "b"), Color, newStr("blue"))
+  prod.addCondition(Var(name: "y"), LeftOf, Var(name: "z"))
+  prod.addCondition(Var(name: "a"), Color, newStr("maize"))
+  prod.addCondition(Var(name: "y"), RightOf, Var(name: "b"))
+  prod.addCondition(Var(name: "x"), Height, Var(name: "h"))
 
-  var session = newSession[Data]()
+  var session = newSession[Data, Attr, Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((newStr("Bob"), newStr("color"), newStr("blue")))
-  session.addFact((newStr("Yair"), newStr("left-of"), newStr("Zach")))
-  session.addFact((newStr("Alice"), newStr("color"), newStr("maize")))
-  session.addFact((newStr("Yair"), newStr("Alice"), newStr("Bob")))
+  session.addFact((newStr("Bob"), Color, newStr("blue")))
+  session.addFact((newStr("Yair"), LeftOf, newStr("Zach")))
+  session.addFact((newStr("Alice"), Color, newStr("maize")))
+  session.addFact((newStr("Yair"), RightOf, newStr("Bob")))
 
-  session.addFact((newStr("Xavier"), newStr("height"), newInt(72)))
-  session.addFact((newStr("Thomas"), newStr("height"), newInt(72)))
-  session.addFact((newStr("Gilbert"), newStr("height"), newInt(72)))
+  session.addFact((newStr("Xavier"), Height, newInt(72)))
+  session.addFact((newStr("Thomas"), Height, newInt(72)))
+  session.addFact((newStr("Gilbert"), Height, newInt(72)))
 
   check prodNode.facts.len == 3
   check prodNode.facts[0].len == 5
-  check vars["a"] == newStr("Alice")
-  check vars["b"] == newStr("Bob")
-  check vars["y"] == newStr("Yair")
-  check vars["z"] == newStr("Zach")
+  check ids["a"] == newStr("Alice")
+  check ids["b"] == newStr("Bob")
+  check ids["y"] == newStr("Yair")
+  check values["z"] == newStr("Zach")
 
 test "adding facts out of order":
-  var vars: Table[string, Data]
-  var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "x"), newStr("on"), Var(name: "y"))
-  prod.addCondition(Var(name: "y"), newStr("left-of"), Var(name: "z"))
-  prod.addCondition(Var(name: "z"), newStr("color"), newStr("red"))
-  prod.addCondition(Var(name: "a"), newStr("color"), newStr("maize"))
-  prod.addCondition(Var(name: "b"), newStr("color"), newStr("blue"))
-  prod.addCondition(Var(name: "c"), newStr("color"), newStr("green"))
-  prod.addCondition(Var(name: "d"), newStr("color"), newStr("white"))
-  prod.addCondition(Var(name: "s"), newStr("on"), newStr("table"))
-  prod.addCondition(Var(name: "y"), Var(name: "a"), Var(name: "b"))
-  prod.addCondition(Var(name: "a"), newStr("left-of"), Var(name: "d"))
+  var ids: Table[string, Data]
+  var attrs: Table[string, Attr]
+  var values: Table[string, Data]
+  var prod = newProduction[Data, Attr, Data](
+    proc (i: Table[string, Data], a: Table[string, Attr], v: Table[string, Data]) =
+      ids = i
+      attrs = a
+      values = v
+  )
+  prod.addCondition(Var(name: "x"), On, Var(name: "y"))
+  prod.addCondition(Var(name: "y"), LeftOf, Var(name: "z"))
+  prod.addCondition(Var(name: "z"), Color, newStr("red"))
+  prod.addCondition(Var(name: "a"), Color, newStr("maize"))
+  prod.addCondition(Var(name: "b"), Color, newStr("blue"))
+  prod.addCondition(Var(name: "c"), Color, newStr("green"))
+  prod.addCondition(Var(name: "d"), Color, newStr("white"))
+  prod.addCondition(Var(name: "s"), On, newStr("table"))
+  prod.addCondition(Var(name: "y"), RightOf, Var(name: "b"))
+  prod.addCondition(Var(name: "a"), LeftOf, Var(name: "d"))
 
-  var session = newSession[Data]()
+  var session = newSession[Data, Attr, Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((newStr("Xavier"), newStr("on"), newStr("Yair")))
-  session.addFact((newStr("Yair"), newStr("left-of"), newStr("Zach")))
-  session.addFact((newStr("Zach"), newStr("color"), newStr("red")))
-  session.addFact((newStr("Alex"), newStr("color"), newStr("maize")))
-  session.addFact((newStr("Bob"), newStr("color"), newStr("blue")))
-  session.addFact((newStr("Charlie"), newStr("color"), newStr("green")))
+  session.addFact((newStr("Xavier"), On, newStr("Yair")))
+  session.addFact((newStr("Yair"), LeftOf, newStr("Zach")))
+  session.addFact((newStr("Zach"), Color, newStr("red")))
+  session.addFact((newStr("Alex"), Color, newStr("maize")))
+  session.addFact((newStr("Bob"), Color, newStr("blue")))
+  session.addFact((newStr("Charlie"), Color, newStr("green")))
  
-  session.addFact((newStr("Seth"), newStr("on"), newStr("table")))
-  session.addFact((newStr("Yair"), newStr("Alex"), newStr("Bob")))
-  session.addFact((newStr("Alex"), newStr("left-of"), newStr("Daniel")))
+  session.addFact((newStr("Seth"), On, newStr("table")))
+  session.addFact((newStr("Yair"), RightOf, newStr("Bob")))
+  session.addFact((newStr("Alex"), LeftOf, newStr("Daniel")))
 
-  session.addFact((newStr("Daniel"), newStr("color"), newStr("white")))
+  session.addFact((newStr("Daniel"), Color, newStr("white")))
 
   check prodNode.facts.len == 1
   check prodNode.facts[0].len == 10
-  check vars["a"] == newStr("Alex")
-  check vars["b"] == newStr("Bob")
-  check vars["y"] == newStr("Yair")
-  check vars["z"] == newStr("Zach")
+  check ids["a"] == newStr("Alex")
+  check ids["b"] == newStr("Bob")
+  check ids["y"] == newStr("Yair")
+  check values["z"] == newStr("Zach")
 
 test "duplicate facts":
-  var vars: Table[string, Data]
-  var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "x"), newStr("self"), Var(name: "y"))
-  prod.addCondition(Var(name: "x"), newStr("color"), newStr("red"))
-  prod.addCondition(Var(name: "y"), newStr("color"), newStr("red"))
+  var ids: Table[string, Data]
+  var attrs: Table[string, Attr]
+  var values: Table[string, Data]
+  var prod = newProduction[Data, Attr, Data](
+    proc (i: Table[string, Data], a: Table[string, Attr], v: Table[string, Data]) =
+      ids = i
+      attrs = a
+      values = v
+  )
+  prod.addCondition(Var(name: "x"), Self, Var(name: "y"))
+  prod.addCondition(Var(name: "x"), Color, newStr("red"))
+  prod.addCondition(Var(name: "y"), Color, newStr("red"))
 
-  var session = newSession[Data]()
+  var session = newSession[Data, Attr, Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((newStr("b1"), newStr("self"), newStr("b1")))
-  session.addFact((newStr("b1"), newStr("color"), newStr("red")))
+  session.addFact((newStr("b1"), Self, newStr("b1")))
+  session.addFact((newStr("b1"), Color, newStr("red")))
 
   check prodNode.facts.len == 1
   check prodNode.facts[0].len == 3
