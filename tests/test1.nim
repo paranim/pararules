25a1341607873fa9e7d68f9f0d4c725e15f6cd3e
oakes <zach@sekao.net>
2020-01-22 16:24:06 -0500

    Use enums

--- tests/test1.nim
+++ tests/test1.nim
@@ -3,84 +3,93 @@ import pararules
 import tables
 import patty
 
+type
+  Person = enum
+    Alice, Bob, Charlie, David, George,
+    Seth, Thomas, Xavier, Yair, Zach
+  Property = enum
+    Color, LeftOf, Height, On, Self
+
 variant Data:
+  Id(idVal: Person)
+  Attr(attrVal: Property)
   Str(strVal: string)
   Int(intVal: int)
 
 test "number of conditions != number of facts":
   var vars: Table[string, Data]
   var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "b"), Str("color"), Str("blue"))
-  prod.addCondition(Var(name: "y"), Str("left-of"), Var(name: "z"))
-  prod.addCondition(Var(name: "a"), Str("color"), Str("maize"))
+  prod.addCondition(Var(name: "b"), Attr(Color), Str("blue"))
+  prod.addCondition(Var(name: "y"), Attr(LeftOf), Var(name: "z"))
+  prod.addCondition(Var(name: "a"), Attr(Color), Str("maize"))
   prod.addCondition(Var(name: "y"), Var(name: "a"), Var(name: "b"))
-  prod.addCondition(Var(name: "x"), Str("height"), Var(name: "h"))
+  prod.addCondition(Var(name: "x"), Attr(Height), Var(name: "h"))
 
   var session = newSession[Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((Str("Bob"), Str("color"), Str("blue")))
-  session.addFact((Str("Yair"), Str("left-of"), Str("Zach")))
-  session.addFact((Str("Alice"), Str("color"), Str("maize")))
-  session.addFact((Str("Yair"), Str("Alice"), Str("Bob")))
+  session.addFact((Id(Bob), Attr(Color), Str("blue")))
+  session.addFact((Id(Yair), Attr(LeftOf), Id(Zach)))
+  session.addFact((Id(Alice), Attr(Color), Str("maize")))
+  session.addFact((Id(Yair), Id(Alice), Id(Bob)))
 
-  session.addFact((Str("Xavier"), Str("height"), Int(72)))
-  session.addFact((Str("Thomas"), Str("height"), Int(72)))
-  session.addFact((Str("Gilbert"), Str("height"), Int(72)))
+  session.addFact((Id(Xavier), Attr(Height), Int(72)))
+  session.addFact((Id(Thomas), Attr(Height), Int(72)))
+  session.addFact((Id(George), Attr(Height), Int(72)))
 
   check prodNode.facts.len == 3
   check prodNode.facts[0].len == 5
-  check vars["a"] == Str("Alice")
-  check vars["b"] == Str("Bob")
-  check vars["y"] == Str("Yair")
-  check vars["z"] == Str("Zach")
+  check vars["a"] == Id(Alice)
+  check vars["b"] == Id(Bob)
+  check vars["y"] == Id(Yair)
+  check vars["z"] == Id(Zach)
 
 test "adding facts out of order":
   var vars: Table[string, Data]
   var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "x"), Str("on"), Var(name: "y"))
-  prod.addCondition(Var(name: "y"), Str("left-of"), Var(name: "z"))
-  prod.addCondition(Var(name: "z"), Str("color"), Str("red"))
-  prod.addCondition(Var(name: "a"), Str("color"), Str("maize"))
-  prod.addCondition(Var(name: "b"), Str("color"), Str("blue"))
-  prod.addCondition(Var(name: "c"), Str("color"), Str("green"))
-  prod.addCondition(Var(name: "d"), Str("color"), Str("white"))
-  prod.addCondition(Var(name: "s"), Str("on"), Str("table"))
+  prod.addCondition(Var(name: "x"), Attr(On), Var(name: "y"))
+  prod.addCondition(Var(name: "y"), Attr(LeftOf), Var(name: "z"))
+  prod.addCondition(Var(name: "z"), Attr(Color), Str("red"))
+  prod.addCondition(Var(name: "a"), Attr(Color), Str("maize"))
+  prod.addCondition(Var(name: "b"), Attr(Color), Str("blue"))
+  prod.addCondition(Var(name: "c"), Attr(Color), Str("green"))
+  prod.addCondition(Var(name: "d"), Attr(Color), Str("white"))
+  prod.addCondition(Var(name: "s"), Attr(On), Str("table"))
   prod.addCondition(Var(name: "y"), Var(name: "a"), Var(name: "b"))
-  prod.addCondition(Var(name: "a"), Str("left-of"), Var(name: "d"))
+  prod.addCondition(Var(name: "a"), Attr(LeftOf), Var(name: "d"))
 
   var session = newSession[Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((Str("Xavier"), Str("on"), Str("Yair")))
-  session.addFact((Str("Yair"), Str("left-of"), Str("Zach")))
-  session.addFact((Str("Zach"), Str("color"), Str("red")))
-  session.addFact((Str("Alex"), Str("color"), Str("maize")))
-  session.addFact((Str("Bob"), Str("color"), Str("blue")))
-  session.addFact((Str("Charlie"), Str("color"), Str("green")))
+  session.addFact((Id(Xavier), Attr(On), Id(Yair)))
+  session.addFact((Id(Yair), Attr(LeftOf), Id(Zach)))
+  session.addFact((Id(Zach), Attr(Color), Str("red")))
+  session.addFact((Id(Alice), Attr(Color), Str("maize")))
+  session.addFact((Id(Bob), Attr(Color), Str("blue")))
+  session.addFact((Id(Charlie), Attr(Color), Str("green")))
  
-  session.addFact((Str("Seth"), Str("on"), Str("table")))
-  session.addFact((Str("Yair"), Str("Alex"), Str("Bob")))
-  session.addFact((Str("Alex"), Str("left-of"), Str("Daniel")))
+  session.addFact((Id(Seth), Attr(On), Str("table")))
+  session.addFact((Id(Yair), Id(Alice), Id(Bob)))
+  session.addFact((Id(Alice), Attr(LeftOf), Id(David)))
 
-  session.addFact((Str("Daniel"), Str("color"), Str("white")))
+  session.addFact((Id(David), Attr(Color), Str("white")))
 
   check prodNode.facts.len == 1
   check prodNode.facts[0].len == 10
-  check vars["a"] == Str("Alex")
-  check vars["b"] == Str("Bob")
-  check vars["y"] == Str("Yair")
-  check vars["z"] == Str("Zach")
+  check vars["a"] == Id(Alice)
+  check vars["b"] == Id(Bob)
+  check vars["y"] == Id(Yair)
+  check vars["z"] == Id(Zach)
 
 test "duplicate facts":
   var vars: Table[string, Data]
   var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "x"), Str("self"), Var(name: "y"))
-  prod.addCondition(Var(name: "x"), Str("color"), Str("red"))
-  prod.addCondition(Var(name: "y"), Str("color"), Str("red"))
+  prod.addCondition(Var(name: "x"), Attr(Self), Var(name: "y"))
+  prod.addCondition(Var(name: "x"), Attr(Color), Str("red"))
+  prod.addCondition(Var(name: "y"), Attr(Color), Str("red"))
 
   var session = newSession[Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((Str("b1"), Str("self"), Str("b1")))
-  session.addFact((Str("b1"), Str("color"), Str("red")))
+  session.addFact((Id(Bob), Attr(Self), Id(Bob)))
+  session.addFact((Id(Bob), Attr(Color), Str("red")))
 
   check prodNode.facts.len == 1
   check prodNode.facts[0].len == 3
@@ -88,19 +97,19 @@ test "duplicate facts":
 test "removing facts":
   var vars: Table[string, Data]
   var prod = newProduction[Data](proc (v: Table[string, Data]) = vars = v)
-  prod.addCondition(Var(name: "b"), Str("color"), Str("blue"))
-  prod.addCondition(Var(name: "y"), Str("left-of"), Var(name: "z"))
-  prod.addCondition(Var(name: "a"), Str("color"), Str("maize"))
+  prod.addCondition(Var(name: "b"), Attr(Color), Str("blue"))
+  prod.addCondition(Var(name: "y"), Attr(LeftOf), Var(name: "z"))
+  prod.addCondition(Var(name: "a"), Attr(Color), Str("maize"))
   prod.addCondition(Var(name: "y"), Var(name: "a"), Var(name: "b"))
 
   var session = newSession[Data]()
   let prodNode = session.addProduction(prod)
-  session.addFact((Str("Bob"), Str("color"), Str("blue")))
-  session.addFact((Str("Yair"), Str("left-of"), Str("Zach")))
-  session.addFact((Str("Alice"), Str("color"), Str("maize")))
-  session.addFact((Str("Yair"), Str("Alice"), Str("Bob")))
+  session.addFact((Id(Bob), Attr(Color), Str("blue")))
+  session.addFact((Id(Yair), Attr(LeftOf), Id(Zach)))
+  session.addFact((Id(Alice), Attr(Color), Str("maize")))
+  session.addFact((Id(Yair), Id(Alice), Id(Bob)))
   check prodNode.facts.len == 1
 
-  session.removeFact((Str("Bob"), Str("color"), Str("blue")))
+  session.removeFact((Id(Bob), Attr(Color), Str("blue")))
   check prodNode.facts.len == 0
 
